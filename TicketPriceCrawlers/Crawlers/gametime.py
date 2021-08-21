7c11384818ef4e0634779a29b4c52cd361f9edb5
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2021-08-21 16:48:22 +0530

    completed gametime crawler

--- TicketPriceCrawlers/Crawlers/gametime.py
+++ TicketPriceCrawlers/Crawlers/gametime.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
-from time import sleep
 from helperFunctions import getPlatform, getUrl
 import json
-import calendar
+from time import sleep
 
 CHROMEDRIVER_LOCATION = getPlatform()
 
@@ -16,14 +15,18 @@ temp_lst = []
 data = {}
 
 def get_team_info():
-    name = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[3]/div/div[5]/a").text 
+    name = ' '.join(driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[2]/div[2]/div/h1").text.split()[:-1])
     event_details = []
     # container = driver.find_elements_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[1]/*")
     container = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[1]")
     teams = container.find_elements_by_tag_name('div')
+    print(name,len(teams))
     for team in teams:
         if team.get_attribute('class') == "_3M6itQDhXMCuHDczUt2vHq _2c2KjM_rU1fn7ZCal1wUIO":
-            # location = team.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[1]/a[1]/div/div/div[2]/div[3]/div/span[5]").text
+            try:
+                location = team.find_elements_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[1]/a[1]/div/div/div[2]/div[3]/div/*")[-1].text
+            except:
+                location = "None"
             day = team.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[1]/a[1]/div/div/div[1]/div[2]").text
             month = team.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[1]/a[1]/div/div/div[1]/div[1]").text
             price = team.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[1]/a[1]/div/div/div[2]/div[1]").text
@@ -33,12 +36,11 @@ def get_team_info():
             eventData = {
                 'Date': {
                     'Day': day.upper(),
-                    # 'Month': calendar.month_name(month.split('/')[0]),
                     'Month': month.split('/')[0],
                     'date': month.split('/')[1]
                 },
                 'title': title,
-                'location': [],
+                'location': location,
                 'price': price
 
                 }
@@ -48,6 +50,7 @@ def get_team_info():
 
 for url in getUrl(2):
     driver.get(url)
+    sleep(2)
     get_team_info()
 
 json_object = json.dumps(data)
@@ -55,29 +58,3 @@ json_object = json.dumps(data)
 # Writing to sample.json
 with open("gametime.json", "w") as outfile:
     outfile.write(json_object)
-
-    """ for team in container:
-    if team.get_attribute('class') == "_3M6itQDhXMCuHDczUt2vHq _2c2KjM_rU1fn7ZCal1wUIO":
-            day = team.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[1]/a[1]/div/div/div[1]/div[2]").text
-            month = team.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[1]/a[1]/div/div/div[1]/div[1]").text
-            price = team.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[1]/a[1]/div/div/div[2]/div[1]").text
-            title = team.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[1]/a[1]/div/div/div[2]/div[2]").text
-            location = team.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/main/div[3]/section[1]/a[1]/div/div/div[2]/div[3]/div/span[5]").text
-
-            eventData = {
-                    'Date': {
-                        'Day': day.upper(),
-                        'Month': calendar.month_name(month.split('/')[0]),
-                        'date': month.split('/')[1]
-                    },
-                    'location': location.split('-')[1],
-                    'title': title,
-                    'price': price
-
-                }
-            event_details.append(eventData)
-    data[name.strip(' ')[0:2]] = event_details """
-
-
-
-
